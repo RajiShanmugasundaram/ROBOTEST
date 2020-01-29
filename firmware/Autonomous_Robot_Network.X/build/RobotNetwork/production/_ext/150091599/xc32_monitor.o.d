45427bdfa362cdbc5f76dfec772c890368530228
Raji Shanmugasundaram <Raji.shanmugasundaram@microchip.com>
2020-01-29 14:29:20 -0700

    first commit

---
+++ firmware/Autonomous_Robot_Network.X/build/RobotNetwork/production/_ext/150091599/xc32_monitor.o.d
@@ -0,0 +1,2 @@
+build/RobotNetwork/production/_ext/150091599/xc32_monitor.o:  \
+ ../src/config/RobotNetwork/stdio/xc32_monitor.c 
---
+++ firmware/Autonomous_Robot_Network.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# RobotNetwork configuration
+CND_ARTIFACT_DIR_RobotNetwork=dist/RobotNetwork/production
+CND_ARTIFACT_NAME_RobotNetwork=Autonomous_Robot_Network.X.production.hex
+CND_ARTIFACT_PATH_RobotNetwork=dist/RobotNetwork/production/Autonomous_Robot_Network.X.production.hex
+CND_PACKAGE_DIR_RobotNetwork=${CND_DISTDIR}/RobotNetwork/package
+CND_PACKAGE_NAME_RobotNetwork=autonomousrobotnetwork.x.tar
+CND_PACKAGE_PATH_RobotNetwork=${CND_DISTDIR}/RobotNetwork/package/autonomousrobotnetwork.x.tar
